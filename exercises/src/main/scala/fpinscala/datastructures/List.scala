1d9e7fca1ed0f2c260b9783814fee10dc58cfaf5
tulth <tulthix@gmail.com>
2020-04-15 10:26:28 -0500

    wip chapter 3

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -57,13 +57,30 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def setHead[A](l: List[A], h: A): List[A] = Cons(h, tail(l))
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(x, xs) => drop(xs, n - 1)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  @annotation.tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) =>
+      if (f(x)) dropWhile(xs, f)
+      else l
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, x:Int)=>1+x)
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
@@ -72,8 +89,20 @@ object List { // `List` companion object. Contains functions for creating and wo
   def main(args: Array[String]): Unit = {
     println("Data Structures List!")
     val l0 = List(1,2,3)
-    println(f"ex3.2 l0: $l0 tail(l0): ${tail(l0)}")
-    println(f"ex3.3 l0: $l0 setHead(l0, 0): ${setHead(l0, 0)}")
+    println(f"l0: $l0")
+    println(f"ex3.2 tail(l0): ${tail(l0)}")
+    println(f"ex3.3 setHead(l0, 0): ${setHead(l0, 0)}")
+    println(f"ex3.4 drop(l0, 1): ${drop(l0, 1)} drop(l0, 2): ${drop(l0, 2)}")
+    val l1 = List(1,2,3,7,5,6)
+    println(f"l1: $l1")
+    println(f"ex3.5 dropWhile(l1, x:Int=>x<4): ${dropWhile(l1, (x:Int)=>x<4)}")
+    println(f"ex3.6 init(l0): ${init(l0)} init(l1): ${init(l1)}")
+    // ex3.7: product always calls itself and it will not stop short
+    // ex3.8
+    // I think it is identity-like, returns the same data
+    // This suggests data constructors are an application of foldRight
+    println(f"ex3.8 foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_)) = List(1,2,3) = ${foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_))}")
+    println(f"ex3.9 length(l0): ${length(l0)} length(l1): ${length(l1)}")
 
   }
 }
